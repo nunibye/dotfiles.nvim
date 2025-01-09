8d42882bb73cf5cbcf2c607d73475124ca1ef8e0
Eliah Reeves <38873297+nunibye@users.noreply.github.com>
2025-01-09 10:22:20 -0800

    flutter config

--- lua/plugins/lang/flutter.lua
+++ lua/plugins/lang/flutter.lua
@@ -1,9 +1,13 @@
 return {
-    {
-      "akinsho/flutter-tools.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        require("flutter-tools").setup{}
-      end,
-    },
-  }
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+      require("flutter-tools").setup({
+        fvm = is_windows,
+      })
+    end,
+  },
+}
+
