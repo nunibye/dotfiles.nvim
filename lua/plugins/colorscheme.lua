8c91a9e83bd895b0965a19ef1d5b77d26a915055
Eliah Reeves <38873297+nunibye@users.noreply.github.com>
2025-01-28 13:34:26 -0800

    reorganized

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,11 @@
--- return {
---     -- add gruvbox
---     { "catppuccin/nvim" },
---
---     -- Configure LazyVim to load gruvbox
---     {
---       "LazyVim/LazyVim",
---       opts = {
---         colorscheme = "catppuccin-mocha",
---       },
---     }
---   }
-
 return {
-  "rockyzhang24/arctic.nvim",
-  priority = 1000,
-  branch = "v2",
-  dependencies = { "rktjmp/lush.nvim" },
-  config = function()
-    vim.cmd("colorscheme arctic")
-  end,
+  {
+    "rockyzhang24/arctic.nvim",
+    priority = 1000,
+    branch = "v2",
+    dependencies = { "rktjmp/lush.nvim" },
+    config = function()
+      vim.cmd("colorscheme arctic")
+    end,
+  },
 }
